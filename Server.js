9948b7ffcd6b77bc63a03b736bac652a049d3fbf
FunbiOyede <oyedetobi49@gmail.com>
2019-12-21 00:00:08 +0100

    added travis

--- Server.js
+++ Server.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const app = require('./App');
+const http = require("http");
+const app = require("./App");
 
 const server = http.createServer(app);
 
-server.listen(9000, () => {
-  console.log('server started');
+server.listen(process.env.PORT || 9000, () => {
+  console.log("server started");
 });
